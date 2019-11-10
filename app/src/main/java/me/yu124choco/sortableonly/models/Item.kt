dc6aadb1741ec9780cf531eaf541d180f93d4e1a
ksprout <adddmoretobe@gmail.com>
2019-11-11 01:42:52 +0900

    Itemモデルを追加した

---
+++ app/src/main/java/me/yu124choco/sortableonly/models/Item.kt
@@ -0,0 +1,22 @@
+package me.yu124choco.sortableonly.models
+
+import androidx.room.*
+
+@Entity
+data class Item(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "name") val name: String?,
+    @ColumnInfo(name = "description") val description: String?
+)
+
+@Dao
+interface ItemDao {
+    @Query("SELECT * FROM item")
+    fun getAll(): List<Item>
+
+    @Insert
+    fun insertAll(items: List<Item>)
+
+    @Delete
+    fun deleteAll(items: List<Item>)
+}
