e5fedcec80c6833099695a81785c276ec29eb53b
ksprout <adddmoretobe@gmail.com>
2019-11-11 02:03:29 +0900

    データベースのインスタンスを返すメソッドを追加

--- app/src/main/java/me/yu124choco/sortableonly/database/AppDatabase.kt
+++ app/src/main/java/me/yu124choco/sortableonly/database/AppDatabase.kt
@@ -1,11 +1,35 @@
 package me.yu124choco.sortableonly.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import me.yu124choco.sortableonly.models.Item
 import me.yu124choco.sortableonly.models.ItemDao
 
 @Database(entities = [Item::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
