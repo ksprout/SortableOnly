76a869ee14765d4a45603fe5485bc985cd0f4778
ksprout <adddmoretobe@gmail.com>
2019-11-11 19:14:43 +0900

    アイテム作成のアクティビティを追加した

--- app/src/main/java/me/yu124choco/sortableonly/MainActivity.kt
+++ app/src/main/java/me/yu124choco/sortableonly/MainActivity.kt
@@ -1,5 +1,7 @@
 package me.yu124choco.sortableonly
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -7,6 +9,7 @@ import android.view.MotionEvent
 import android.view.animation.TranslateAnimation
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import me.yu124choco.sortableonly.activities.ItemCreateActivity
 import me.yu124choco.sortableonly.database.AppDatabase
 import me.yu124choco.sortableonly.fragments.ItemsListFragment
 import me.yu124choco.sortableonly.util.makeShortToast
@@ -14,11 +17,24 @@ import me.yu124choco.sortableonly.models.Item
 
 class MainActivity : AppCompatActivity() {
 
+    private val RESULT_ITEM_CREATE = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupClickListeners()
         updateItemsList()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                RESULT_ITEM_CREATE -> {
+                    
+                }
+            }
+        }
     }
 
     private fun updateItemsList() {
@@ -32,7 +48,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListeners() {
         button_add_item.setOnClickListener {
-
+            Handler().postDelayed({
+                val intent = Intent(application, ItemCreateActivity::class.java)
+                startActivityForResult(intent, RESULT_ITEM_CREATE)
+            }, 500)
         }
 
         // ホバーアニメーションを設定
---
+++ app/src/main/java/me/yu124choco/sortableonly/activities/ItemCreateActivity.kt
@@ -0,0 +1,15 @@
+package me.yu124choco.sortableonly.activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import me.yu124choco.sortableonly.R
+
+class ItemCreateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_create)
+    }
+
+}
