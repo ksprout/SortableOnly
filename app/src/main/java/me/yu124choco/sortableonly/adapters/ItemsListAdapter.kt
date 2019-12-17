ed168b4beb5824e43f6b79b091e6d925bde044bb
ksprout <adddmoretobe@gmail.com>
2019-12-17 11:49:39 +0900

    ListViewをRecyclerViewに置き換えた

--- app/src/main/java/me/yu124choco/sortableonly/adapters/ItemsListAdapter.kt
+++ app/src/main/java/me/yu124choco/sortableonly/adapters/ItemsListAdapter.kt
@@ -8,56 +8,41 @@ import android.widget.BaseAdapter
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
 import me.yu124choco.sortableonly.R
 import me.yu124choco.sortableonly.models.Item
 
-class ItemsListAdapter(private val context: Context, private val items: MutableList<Item>, private val onItemClickListener: ((item: Item) -> Unit)) : BaseAdapter() {
+class ItemsListAdapter(private val context: Context, private val items: MutableList<Item>, private val onItemClickListener: ((item: Item) -> Unit)) : RecyclerView.Adapter<ItemsListAdapter.ViewHolder>() {
 
-    private class ViewHolder {
-        var itemContainer: ConstraintLayout? = null
-        var checkBox: CheckBox? = null
-        var textViewName: TextView? = null
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var itemContainer: ConstraintLayout = view.findViewById(R.id.item_container)
+        var checkBox: CheckBox = view.findViewById(R.id.item_check_box)
+        var textViewName: TextView = view.findViewById(R.id.text_view_name)
     }
 
     val checkedItems: MutableList<Item> = mutableListOf()
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val holder: ViewHolder
-        var cv = convertView
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.adapter_items_list, parent, false)
+        return ViewHolder(view)
+    }
 
-        if (cv == null) {
-            cv = LayoutInflater.from(context).inflate(R.layout.adapter_items_list, null)
-            holder = ViewHolder()
-            holder.itemContainer = cv.findViewById(R.id.item_container)
-            holder.checkBox = cv.findViewById(R.id.item_check_box)
-            holder.textViewName = cv.findViewById(R.id.text_view_name)
-            cv.tag = holder
-        } else {
-            holder = cv.tag as ViewHolder
-        }
-        holder.itemContainer?.setOnClickListener {
-            onItemClickListener.invoke(items[position])
-        }
-        holder.checkBox?.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked && checkedItems.firstOrNull { it.id == items[position].id } == null) {
-                checkedItems.add(items[position])
-            } else if (!isChecked) {
-                checkedItems.removeAll { it.id == items[position].id }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.let { h ->
+            h.itemContainer.setOnClickListener {
+                onItemClickListener.invoke(items[position])
             }
+            h.checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked && checkedItems.firstOrNull { it.id == items[position].id } == null) {
+                    checkedItems.add(items[position])
+                } else if (!isChecked) {
+                    checkedItems.removeAll { it.id == items[position].id }
+                }
+            }
+            h.textViewName.text = items[position].name
         }
-        holder.textViewName?.text = items[position].name
-        return cv!!
     }
 
-    override fun getCount(): Int {
-        return items.size
-    }
-
-    override fun getItem(position: Int): Any {
-        return position
-    }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
+    override fun getItemCount(): Int = items.size
 }
--- app/src/main/java/me/yu124choco/sortableonly/fragments/ItemsListFragment.kt
+++ app/src/main/java/me/yu124choco/sortableonly/fragments/ItemsListFragment.kt
@@ -8,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_items_list.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -61,6 +63,7 @@ class ItemsListFragment : Fragment() {
             if (onItemsListElemClickListener != null) onItemsListElemClickListener?.invoke(item)
         }
         list_view_items?.adapter = itemsListAdapter
+        list_view_items?.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
     }
 
     fun deleteTargetItems(activity: Activity) = GlobalScope.launch(Dispatchers.Main) {
