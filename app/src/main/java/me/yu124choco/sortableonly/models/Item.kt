a21c8557c726098c84c49e0b5c39279072d0230b
ksprout <adddmoretobe@gmail.com>
2020-01-01 19:27:37 +0900

    Item新規追加のメソッドを修正した

--- app/src/main/java/me/yu124choco/sortableonly/models/Item.kt
+++ app/src/main/java/me/yu124choco/sortableonly/models/Item.kt
@@ -7,7 +7,7 @@ data class Item(
     @PrimaryKey(autoGenerate = true) var id: Int?,
     @ColumnInfo(name = "name") var name: String?,
     @ColumnInfo(name = "description") var description: String?,
-    @ColumnInfo(name = "order") var order: Int?
+    @ColumnInfo(name = "order_number") var orderNumber: Long?
 )
 
 @Dao
@@ -18,8 +18,21 @@ interface ItemDao {
     @Query("SELECT * FROM items")
     fun getAll(): List<Item>
 
+    @Query("SELECT MAX(order_number) FROM items")
+    fun getMaxOrderNumber(): Long
+
     @Insert
-    fun insertAll(items: List<Item>)
+    fun insertAllMain(items: List<Item>)
+
+    @Transaction
+    fun insertAll(items: List<Item>) {
+        var max = getMaxOrderNumber() + 1
+        for (item in items) {
+            item.orderNumber = max
+            max++
+        }
+        insertAllMain(items)
+    }
 
     @Update
     fun updateAll(items: List<Item>)
